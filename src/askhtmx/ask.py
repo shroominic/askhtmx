42af6ee767a23bfc887a7e80f1b8563a30927844
Shroominic <db2002dominic@gmail.com>
2023-11-14 19:18:59 +0100

    🗑️ Remove ask.py file

--- src/askhtmx/ask.py
+++
@@ -1,9 +0,0 @@
-from funcchain import chain
-
-
-def ask_htmx(customer_question: str) -> str:
-    """
-    Answer the question about htmx. If the question is not about htmx, then
-    tell the customer that we only answer questions about htmx.
-    """
-    return chain()
